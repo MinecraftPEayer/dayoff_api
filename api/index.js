5ff4d7a9880768116747312cce06b8056d9b9059
MinecraftPEayer <boingli227@gmail.com>
2024-10-01 18:50:34 +0800

    嘗試新增embed

--- api/index.js
+++ api/index.js
@@ -1,6 +1,31 @@
 const jsdom = require("jsdom");
 const express = require('express')
 
+let code = {
+    'tp': '臺北市',
+    'ntpc': '新北市',
+    'kl': '基隆市',
+    'tyc': '桃園市',
+    'hc': '新竹市',
+    'hcc': '新竹縣',
+    'mlc': '苗栗縣',
+    'tc': '臺中市',
+    'ntct': '南投縣',
+    'ylc': '雲林縣',
+    'cyc': '嘉義縣',
+    'cy': '嘉義市',
+    'tn': '臺南市',
+    'kh': '高雄市',
+    'ptc': '屏東縣',
+    'hlc': '花蓮縣',
+    'ttct': '臺東縣',
+    'phc': '澎湖縣',
+    'km': '金門縣',
+    'matsu': '連江縣',
+    'ilc': '宜蘭縣',
+    'chc': '彰化縣'
+}
+
 const app = express()
 
 app.get("/", (req, res) => res.send("<style>html { width: 100%; height: 100%; } body { background-color: rgb(43, 43, 43); font-family: sans-serif; }</style><a href=\"/api/typhoon\" style=\"color: white\">https://typhoon-api.vercel.app/api/typhoon</a>"));
@@ -29,6 +54,34 @@ app.get('/api/typhoon', async (req, res) => {
     }
 
     res.json(data)
+})
+
+app.get('/status/:city', async (req, res) => {
+    let resp = await fetch('https://www.dgpa.gov.tw/typh/daily/nds.html', {
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8'
+        }
+    })
+
+    let html = await resp.text()
+
+    let dom = new jsdom.JSDOM(html)
+    let document = dom.window.document
+    
+    const CityName = document.querySelectorAll('[headers="city_Name"]');
+    const Status = document.querySelectorAll('[headers="StopWorkSchool_Info"]');
+
+    let data = []
+    for (let i = 0; i < CityName.length; i++) {
+        data.push({
+            cityName: CityName[i].textContent,
+            status: Status[i].textContent.replace(/  /g, '\n').slice(0, -1)
+        })
+    }
+
+    let dataReturn = data.filter((data) => data.cityName === code[req.params.city])[0]
+
+    res.send(`<html><head><title>Website Name</title><meta property="og:type" content="website"><meta property="og:url" content=""><meta property="og:title" content="${dataReturn.cityName}"><meta property="og:description" content="${dataReturn.status}\n\n資料來源: https://www.dgpa.gov.tw/typh/daily/nds.html"></head></html>`)
 })
 
 app.listen(3000, () => {
